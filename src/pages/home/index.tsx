0cb11c4f0f385712ac886b4aba3f46cba489d625
mangonob <mangonob@qq.com>
2022-03-27 11:29:17 +0800

    feat: css module

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,20 +2,25 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Post } from "../posts/post-slice";
 
+import styles from "./index.module.css";
+
 export default function Home() {
   const posts = useSelector<any, Post[]>((s) => (s as any).posts);
 
   return (
-    <ul>
-      {posts.map((post) => {
-        const { title, content } = post;
-        return (
-          <li>
-            <p>{title}</p>
-            {content && <p>{content}</p>}
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <div className={styles.block}></div>
+      <ul>
+        {posts.map((post) => {
+          const { id, title, content } = post;
+          return (
+            <li key={id}>
+              <p>{title}</p>
+              {content && <p>{content}</p>}
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
