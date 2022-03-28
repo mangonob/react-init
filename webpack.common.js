de7f3a9952bb5f375dad5f6dafa8496681d86b23
mangonob <mangonob@qq.com>
2022-03-29 00:32:06 +0800

    feat: load resources

--- webpack.common.js
+++ webpack.common.js
@@ -56,6 +56,14 @@ module.exports = {
           "less-loader",
         ],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   resolve: {
