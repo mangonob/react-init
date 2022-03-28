c06f74b294f1bf8e3e0ab8a71ccc636b2ba8e5e4
mangonob <mangonob@qq.com>
2022-03-29 04:18:06 +0800

    feat: Update post

--- src/pages/posts/edit-post.tsx
+++ src/pages/posts/edit-post.tsx
@@ -1,28 +1,42 @@
-import { Form, Input } from 'antd';
+import { nanoid } from '@reduxjs/toolkit';
+import { Button, Form, Input } from 'antd';
 import { useForm } from 'antd/lib/form/Form';
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
-import { RootState } from 'src/store';
-import { Post } from './post-slice';
+import { AppDispatch, RootState } from 'src/store';
+import { Post, postAdded, postUpdate } from './post-slice';
 
 export default function EditPost() {
   const [form] = useForm<Post>();
   const { postId } = useParams();
+  const dispatch = useDispatch<AppDispatch>();
   const post = useSelector<RootState, Post | undefined>((s) =>
     s.posts.find((p) => p.id === postId)
   );
 
-  // const {} = post;
+  const { id, title, content } = post || {};
+
+  const onSave = useCallback(async () => {
+    const values = await form.validateFields();
+    if (id) {
+      dispatch(postUpdate({ id, ...values }));
+    } else {
+      dispatch(postAdded({ id: nanoid(), ...values }));
+    }
+  }, [form, dispatch, id]);
 
   return (
-    <Form form={form}>
-      <Form.Item name="title" label="Title">
-        <Input></Input>
-      </Form.Item>
-      <Form.Item name="content" label="Content">
-        <Input.TextArea></Input.TextArea>
-      </Form.Item>
-    </Form>
+    <>
+      <Form form={form} initialValues={{ title, content }}>
+        <Form.Item name="title" label="Title" rules={[{ required: true }]}>
+          <Input></Input>
+        </Form.Item>
+        <Form.Item name="content" label="Content" rules={[{ required: true }]}>
+          <Input.TextArea></Input.TextArea>
+        </Form.Item>
+      </Form>
+      <Button onClick={onSave}>{(id && 'Save') || 'Create Post'}</Button>
+    </>
   );
 }
