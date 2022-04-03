85ce21140c2cfa1a7581bee9be9622b3b15b52e2
mangonob <mangonob@qq.com>
2022-04-04 03:17:55 +0800

    feat: disable edit mode

--- src/pages/posts/post-list/index.tsx
+++ src/pages/posts/post-list/index.tsx
@@ -1,8 +1,12 @@
-import { MinusCircleOutlined, PlusCircleOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Form, Space, Spin } from 'antd';
-import { FormInstance, useForm } from 'antd/lib/form/Form';
-import produce from 'immer';
-import React, { memo, useEffect, useMemo, useState } from 'react';
+import {
+  MinusCircleOutlined,
+  PlusCircleOutlined,
+  SyncOutlined,
+} from '@ant-design/icons';
+import { Button, Spin } from 'antd';
+import { FormInstance } from 'antd/lib/form/Form';
+import classNames from 'classnames';
+import React, { memo, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { AppDispatch, RootState } from 'src/store';
@@ -21,113 +25,61 @@ export default function PostList() {
   );
   const dispatch = useDispatch<AppDispatch>();
 
-  const [isEditing, setEditing] = useState(false);
-  const [valuesDep, toggleValues] = useState({});
-
-  const [form] = useForm<{ selected?: { [key in string]: boolean } }>();
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const formValues = useMemo(() => form.getFieldsValue(), [form, valuesDep]);
-
   useEffect(() => {
     if (postStatus === 'idle') {
       dispatch(fetchPosts());
     }
   }, [postStatus, dispatch]);
 
-  const isAllSelected = useMemo(() => {
-    return (
-      Object.entries(formValues.selected || {}).filter(
-        ([, isSelected]) => isSelected
-      ).length === posts.length
-    );
-  }, [formValues, posts.length]);
-
-  const onSelectAll = useMemo(() => {
-    return (isSelected: boolean) => {
-      form.setFieldsValue({
-        selected: produce({} as { [key in string]: boolean }, (draft) => {
-          // eslint-disable-next-line unicorn/no-array-for-each
-          posts.forEach(({ id }) => {
-            draft[id] = isSelected;
-          });
-        }),
-      });
-      toggleValues({});
-    };
-  }, [form, posts]);
+  const isLoading = postStatus === 'loading';
 
   return (
     <section className={styles.postList}>
-      <h2>Posts</h2>
-      <Form form={form} onValuesChange={() => toggleValues({})}>
-        <ul>
-          {isEditing && (
-            <li key="selectAll">
-              <Checkbox
-                checked={isAllSelected}
-                className={styles.checkbox}
-                onChange={(e) => {
-                  onSelectAll(e.target.checked);
-                }}
-              ></Checkbox>
-              <a>Select All</a>
-            </li>
-          )}
-          <Spin spinning={postStatus === 'loading'}>
-            {posts.map((post) => (
-              <_PostItem post={post} isEditing={isEditing} key={post.id} />
-            ))}
-          </Spin>
-        </ul>
-      </Form>
-      <Space>
+      <h2>
+        Posts:
         <Link to="/create">
-          <Button icon={<PlusCircleOutlined />}>Add Post</Button>
-        </Link>
-        {(isEditing || posts.length > 0) && (
-          <Button
-            type={isEditing ? 'default' : 'primary'}
-            onClick={() => {
-              if (isEditing) {
-                onSelectAll(false);
-              }
-              setEditing(!isEditing);
-            }}
-          >
-            {isEditing ? 'Cancel' : 'Edit'}
+          <Button className={styles.addPost} icon={<PlusCircleOutlined />}>
+            Add Post
           </Button>
-        )}
-        {isEditing && <Button type="primary">Remove Selected</Button>}
-      </Space>
+        </Link>
+        <SyncOutlined
+          className={classNames(styles.refresh, {
+            [styles.disabled]: isLoading,
+          })}
+          onClick={() => {
+            if (!isLoading) {
+              dispatch(fetchPosts());
+            }
+          }}
+        />
+      </h2>
+      <Spin spinning={isLoading}>
+        <ul>
+          {posts.map((post) => (
+            <_PostItem post={post} key={post.id} />
+          ))}
+        </ul>
+      </Spin>
     </section>
   );
 }
 
-function PostItem(props: { post: Post; isEditing?: boolean }) {
-  const { post, isEditing = false } = props;
+function PostItem(props: { post: Post }) {
+  const { post } = props;
   const { id, title } = post;
 
   const dispatch = useDispatch<AppDispatch>();
 
   return (
     <li>
-      {isEditing && (
-        <Form.Item name={['selected', id]} noStyle valuePropName="checked">
-          <Checkbox className={styles.checkbox} />
-        </Form.Item>
-      )}
-      {!isEditing && <Link to={`/post/${id}`}>{title}</Link>}
-      {isEditing && <a>{title}</a>}
-      {isEditing && (
-        <MinusCircleOutlined
-          onClick={(e) => {
-            e.stopPropagation();
-            dispatch(removePost(id));
-          }}
-          style={{ color: 'red', marginLeft: 8 }}
-        />
-      )}
+      <Link to={`/post/${id}`}>{title}</Link>
+      <MinusCircleOutlined
+        onClick={(e) => {
+          e.stopPropagation();
+          dispatch(removePost(id));
+        }}
+        style={{ color: 'red', marginLeft: 8 }}
+      />
     </li>
   );
 }
--- src/pages/posts/thunk.ts
+++ src/pages/posts/thunk.ts
@@ -22,7 +22,7 @@ async function _fetchFakePosts(): Promise<Post[]> {
         }
       );
       resolve(posts);
-    }, 1000);
+    }, 200);
   });
 }
 
