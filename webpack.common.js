f09a106cbed0ff9f22e8751dc0fbede49bc9d1ac
mangonob <mangonob@qq.com>
2022-04-09 17:30:22 +0800

    feat: static

--- webpack.common.js
+++ webpack.common.js
@@ -63,6 +63,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'static/[hash][ext][query]',
+        },
       },
     ],
   },
