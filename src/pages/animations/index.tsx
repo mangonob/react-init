388222ec6a78ee07c469ad2ab4d3300a349479db
mangonob <mangonob@qq.com>
2022-05-29 01:54:23 +0800

    feat: grid

--- src/pages/animations/index.tsx
+++ src/pages/animations/index.tsx
@@ -19,21 +19,33 @@ export default function Animations() {
   useEffect(() => {
     ani.current = anime({
       targets: `.${styles.item}`,
-      translateX: '200px',
       autoplay: false,
-      rotate: anime.stagger([-360, 360]),
-      delay: anime.stagger(50, { direction: 'reverse', easing: 'easeOutQuad' }),
+      scale: [
+        { value: 0.1, easing: 'easeOutSine', duration: 500 },
+        { value: 1, easing: 'easeInOutQuad', duration: 1200 },
+      ],
+      delay: anime.stagger(200, { grid: [14, 5], from: 'center' }),
     });
   });
+
+  const renderRow = (length: number) => {
+    return Array.from({ length }).map((_, index) => (
+      <div className={styles.item} key={index}></div>
+    ));
+  };
+
+  const renderGrid = (row: number, column: number) => {
+    return Array.from({ length: row }).map((_, index) => (
+      <div key={index} className={styles.row}>
+        {renderRow(column)}
+      </div>
+    ));
+  };
 
   return (
     <div className={styles.animations}>
       <Button onClick={onClick}>Reverse</Button>
-      {Array.from({ length: 12 })
-        .fill(0)
-        .map((_, index) => (
-          <div className={styles.item} key={index}></div>
-        ))}
+      <div className={styles.grid}>{renderGrid(5, 14)}</div>
     </div>
   );
 }
