a55291e9883513e10c7c1ef9ebd64a6ea79c860f
mangonob <mangonob@qq.com>
2022-05-29 01:44:16 +0800

    feat: stagger

--- src/pages/animations/index.tsx
+++ src/pages/animations/index.tsx
@@ -19,27 +19,17 @@ export default function Animations() {
   useEffect(() => {
     ani.current = anime({
       targets: `.${styles.item}`,
-      borderRadius: ['20%', '50%'],
-      rotate() {
-        return anime.random(-360, 360);
-      },
-      duration: 1000,
+      translateX: '200px',
       autoplay: false,
-      width: '*=2',
-      height: '*=2',
-      marginLeft: '300px',
-      delay: (el, i) => {
-        console.log(el);
-        return i * 100;
-      },
-      easing: 'easeOutQuad',
+      rotate: anime.stagger([-360, 360]),
+      delay: anime.stagger(50, { direction: 'reverse', easing: 'easeOutQuad' }),
     });
   });
 
   return (
     <div className={styles.animations}>
       <Button onClick={onClick}>Reverse</Button>
-      {Array.from({ length: 4 })
+      {Array.from({ length: 12 })
         .fill(0)
         .map((_, index) => (
           <div className={styles.item} key={index}></div>
