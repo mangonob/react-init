1505b59beb15757cbf84e790685d008474de7201
mangonob <mangonob@qq.com>
2022-05-31 22:22:24 +0800

    feat: progress

--- src/pages/animations/index.tsx
+++ src/pages/animations/index.tsx
@@ -1,39 +1,60 @@
+import { PauseCircleOutlined, PlayCircleOutlined } from '@ant-design/icons';
 import anime, { AnimeInstance } from 'animejs';
-import { Button } from 'antd';
-import React, { useCallback, useEffect, useRef } from 'react';
+import { Button, Slider } from 'antd';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styles from './index.module.scss';
 
 export default function Animations() {
   const ani = useRef<AnimeInstance>();
-  const isPlayed = useRef(false);
+  const [isPlayed, setPlay] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   const onClick = useCallback(() => {
     const maybeAni = ani.current;
     if (maybeAni) {
-      if (isPlayed.current) maybeAni.reverse();
-      isPlayed.current = true;
-      maybeAni.play();
+      if (isPlayed) {
+        maybeAni.pause();
+      } else {
+        maybeAni.play();
+      }
+      setPlay(!isPlayed);
     }
-  }, []);
+  }, [isPlayed]);
 
   useEffect(() => {
     const animation = anime.timeline({
       autoplay: false,
       targets: `.${styles.item}`,
       easing: 'easeOutExpo',
+      duration: 250,
+      update(ani) {
+        setProgress(ani.progress);
+      },
     });
     ani.current = animation;
 
-    animation.add({
-      translateX: 300,
-    });
-
-    animation.add({
-      translateY: 100,
-      width: '+=100',
-      height: '+=100',
-      borderRadius: '50%',
-    });
+    animation
+      .add({
+        translateX: 0,
+        translateY: 0,
+        width: 20,
+        height: 20,
+      })
+      .add({
+        translateX: 300,
+        translateY: 0,
+      })
+      .add({
+        translateY: 100,
+        width: 100,
+        height: 100,
+        borderRadius: '50%',
+      })
+      .add({
+        translateX: 0,
+        width: 20,
+        height: 20,
+      });
   });
 
   const renderRow = (length: number) => {
@@ -52,7 +73,19 @@ export default function Animations() {
 
   return (
     <div className={styles.animations}>
-      <Button onClick={onClick}>Reverse</Button>
+      <Button
+        onClick={onClick}
+        icon={isPlayed ? <PauseCircleOutlined /> : <PlayCircleOutlined />}
+      ></Button>
+      <Slider
+        value={progress}
+        onChange={(value) => {
+          const maybeAni = ani.current;
+          if (maybeAni) {
+            maybeAni.seek((maybeAni.duration * value) / 100);
+          }
+        }}
+      ></Slider>
       <div className={styles.grid}>{renderGrid(1, 1)}</div>
     </div>
   );
