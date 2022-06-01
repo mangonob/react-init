da42cfa275e07dfaddd756615ab6aae66b21983e
mangonob <mangonob@qq.com>
2022-06-02 00:02:02 +0800

    feat: clean up

--- webpack.common.js
+++ webpack.common.js
@@ -1,33 +1,11 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-const webpack = require('webpack');
 const path = require('path');
-
-const cssModule = {
-  loader: 'css-loader',
-  options: {
-    modules: {
-      auto: true,
-      exportLocalsConvention: 'camelCaseOnly',
-      localIdentName: '[hash:base64]__[local]',
-    },
-  },
-};
 
 /** @type { import("webpack").Configuration } */
 module.exports = {
   entry: {
-    index: './src/index.tsx',
+    index: './src/index.ts',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new webpack.ContextReplacementPlugin(
-      /moment[/\\]locale$/,
-      /zh-cn|zh-hk|en/
-    ),
-  ],
   module: {
     rules: [
       {
@@ -39,34 +17,6 @@ module.exports = {
         test: /\.tsx?$/,
         loader: 'ts-loader',
         exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', cssModule],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader', cssModule, 'sass-loader'],
-      },
-      {
-        test: /\.less$/i,
-        use: [
-          // compiles Less to CSS
-          'style-loader',
-          'css-loader',
-          'less-loader',
-        ],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'static/[hash][ext][query]',
-        },
       },
     ],
   },
