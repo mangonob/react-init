f048a542856e40ad8f6fdc8578946a0159f8719a
mangonob <mangonob@qq.com>
2023-01-12 17:19:07 +0800

    feat: download

--- src/pages/playground/index.tsx
+++ src/pages/playground/index.tsx
@@ -1,18 +1,41 @@
-import { Tabs } from 'antd';
-import React from 'react';
-import FlowChartExample from 'src/components/flow-chart/example';
-import { ZustandExmaple } from '../examples';
+import { Button, Form, Select } from 'antd';
+import React, { useMemo } from 'react';
+import { useLocalStorage } from 'react-use';
+
 import styles from './index.module.scss';
 
 export default function Playground() {
+  const [env, setEnv] = useLocalStorage('storage:env', 'test');
+  const downloadURL = useDownloadURL(env || 'test');
+
   return (
-    <Tabs defaultActiveKey={'0'} className={styles.playground}>
-      <Tabs.TabPane tab="React Flow" key="0">
-        <FlowChartExample />
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="Zustand" key="1">
-        <ZustandExmaple />
-      </Tabs.TabPane>
-    </Tabs>
+    <div className={styles.download}>
+      <Form>
+        <Form.Item label="Environment">
+          <Select
+            options={[
+              { value: 'test', label: 'Test' },
+              { value: 'uat', label: 'Uat' },
+            ]}
+            onChange={(env) => setEnv(env)}
+            defaultValue={env}
+          ></Select>
+        </Form.Item>
+        <Button
+          type="primary"
+          onClick={() => {
+            window.location.href = downloadURL;
+          }}
+        >
+          Download
+        </Button>
+      </Form>
+    </div>
   );
+}
+
+function useDownloadURL(env: string): string {
+  return useMemo(() => {
+    return `itms-services://?action=download-manifest&url=https://vstone-asa.oss-cn-hangzhou.aliyuncs.com/package/${env.toLowerCase()}/manifest.plist`;
+  }, [env]);
 }
