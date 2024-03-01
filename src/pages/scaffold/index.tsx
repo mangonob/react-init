af45343ed4ca5d0c5d835cb2662a74d8b0222f05
mangonob <mangonob@qq.com>
2024-03-01 10:55:19 +0800

    feat: framer-motion

--- src/pages/scaffold/index.tsx
+++ src/pages/scaffold/index.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import { SwapOutlined } from '@ant-design/icons';
+import { Button, Space } from 'antd';
+import { AnimatePresence, motion } from 'framer-motion';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router';
 
 export default function Scaffold() {
+  const [inOrder, setInOrder] = useState(false);
+
   return (
     <>
-      Scaffold
+      <Space direction="vertical">
+        <Button
+          onClick={() => {
+            setInOrder(!inOrder);
+          }}
+          type="primary"
+        >
+          交换方向
+        </Button>
+        <AnimatePresence>
+          <Space>
+            <motion.div layout key={inOrder ? 'aaa' : 'bbb'}>
+              {inOrder ? 'AAAAAAAAAA' : 'BBB'}
+            </motion.div>
+            <motion.div layout>
+              <SwapOutlined />
+            </motion.div>
+            <motion.div layout key={inOrder ? 'bbb' : 'aaa'}>
+              {inOrder ? 'BBB' : 'AAAAAAAAAA'}
+            </motion.div>
+          </Space>
+        </AnimatePresence>
+      </Space>
       <Outlet />
     </>
   );
