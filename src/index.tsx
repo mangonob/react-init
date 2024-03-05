62ba95987a93a2266f843906e9031e53d8ff5533
mangonob <mangonob@qq.com>
2024-03-05 15:09:30 +0800

    feat: #body not null

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-createRoot(document.querySelector('#root') ?? document.body).render(<App />);
+createRoot(document.querySelector('#root') as Element).render(<App />);
