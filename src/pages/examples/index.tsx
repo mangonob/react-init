bfe97aea7c05ed6606a384a714222b9d18bee8ed
mangonob <mangonob@qq.com>
2024-04-19 17:06:06 +0800

    feat: cleanup

--- src/pages/examples/index.tsx
+++ src/pages/examples/index.tsx
@@ -1,19 +1,18 @@
 import {
-  QueryKey,
+  useQueryClient,
   useQueryErrorResetBoundary,
   useSuspenseQuery,
 } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Button, Flex, Spin } from 'antd';
-import React, { Suspense, memo, useState } from 'react';
+import React, { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
-import { Prefixed } from 'src/foundation/array';
 
 import styles from './index.module.scss';
 
 export default function Examples() {
   const { reset } = useQueryErrorResetBoundary();
-  const [open, setOpen] = useState(false);
+  const queryClient = useQueryClient();
 
   return (
     <>
@@ -29,33 +28,18 @@ export default function Examples() {
               <MyComponents />
             </Suspense>
           </ErrorBoundary>
-          <Button onClick={() => setOpen(!open)}>
-            {open ? 'Opened' : 'Closed'}{' '}
+          <Button
+            onClick={() =>
+              queryClient.invalidateQueries({ queryKey: ['demo'] })
+            }
+          >
+            Invalid
           </Button>
-          {Array.from({ length: 100 }).map((_, i) => {
-            return <_Foo key={`foo-${i}`} label={i.toString()} />;
-          })}
         </Flex>
       </div>
       <ReactQueryDevtools />
     </>
   );
-}
-
-export type Reducer<S, Action> = (s: S, action: Action) => S;
-
-export function combineReducer<S, Action1, Action2>(
-  reducer1: Reducer<S, Action1>,
-  reducer2: Reducer<S, Action2>
-): Reducer<S, Action1 | Action2> {
-  return (s, a) => reducer2(reducer1(s, a as Action1), a as Action2);
-}
-
-export function generateQueryKey<
-  TQueryKey extends QueryKey = QueryKey,
-  P = unknown,
->(queryKey: TQueryKey, p: P): Prefixed<TQueryKey, P> {
-  return [p, ...queryKey];
 }
 
 function MyComponents() {
@@ -64,8 +48,8 @@ function MyComponents() {
     queryFn: ({ signal }) => {
       return new Promise<number>((resolve, reject) => {
         const t = setTimeout(() => {
-          reject(new Error('42'));
-        }, 1500);
+          reject(42);
+        }, 300);
 
         signal.addEventListener('abort', () => {
           clearTimeout(t);
@@ -78,10 +62,3 @@ function MyComponents() {
 
   return <h1>Loaded count: {data}</h1>;
 }
-
-const Foo: React.FC<{ label: string }> = (props) => {
-  const { label } = props;
-  return <span>Foo: {label}</span>;
-};
-
-const _Foo = memo(Foo);
