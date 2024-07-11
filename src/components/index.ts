9afd87aab4bd6eabf076a7d80b698d218ea7c10a
mangonob <mangonob@qq.com>
2024-07-11 11:35:04 +0800

    feat: remove value builder

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,3 @@ export * from './text-diff';
 export * from './redirect';
 export * from './page';
 export { default as Block } from './block';
-export { default as ValueBuilder } from './value-builder';
--- src/pages/examples/index.tsx
+++ src/pages/examples/index.tsx
@@ -1,37 +1,16 @@
-import { Flex, Form, Input, Switch } from 'antd';
-import React, { useDeferredValue, useState } from 'react';
-import { ValueBuilder } from 'src/components';
+import { Form, Input, Switch } from 'antd';
+import React, { useState } from 'react';
 
 export default function Examples() {
-  const [_showItems, setShowItems] = useState(false);
   const [showTitle, setShowTitle] = useState(false);
-  const showItems = useDeferredValue(_showItems);
-
-  const renderItems = () => {
-    return (
-      <Flex wrap="wrap">
-        {Array.from({ length: 20_000 }).map((_, i) => {
-          return (
-            <ValueBuilder<number> value={i} key={i}>
-              {(i) => <span>Item {i}</span>}
-            </ValueBuilder>
-          );
-        })}
-      </Flex>
-    );
-  };
 
   return (
     <Form>
       <Form.Item label="Show title">
         <Switch value={showTitle} onClick={setShowTitle} />
       </Form.Item>
-      <Form.Item label="Show items">
-        <Switch value={showItems} onClick={setShowItems} />
-      </Form.Item>
       {showTitle && <h5>Title</h5>}
       <Input />
-      {showItems && renderItems()}
     </Form>
   );
 }
