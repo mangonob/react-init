180ceb9aeafcc3e67c253fd420d8dca65fb1bde1
mangonob <mangonob@qq.com>
2024-07-19 00:16:47 +0800

    feat: menu click

--- src/components/scaffold/system-menu/index.tsx
+++ src/components/scaffold/system-menu/index.tsx
@@ -1,6 +1,7 @@
 import { Menu, MenuProps } from 'antd';
 import { isArray } from 'lodash-es';
 import React, { useMemo } from 'react';
+import { useLocation, useNavigate } from 'react-router';
 import menus from 'src/routes/menus.yaml';
 
 interface SystemMenuProps extends MenuProps {}
@@ -19,14 +20,28 @@ type MenuItem = {
 );
 
 export default function SystemMenu(props: SystemMenuProps) {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
   const items = useMemo((): NonNullable<MenuProps['items']> => {
     const _menus = menus as MenuItem[];
     return isArray(_menus)
-      ? _menus.map((m) => {
-          return { label: m.title } as NonNullable<MenuProps['items']>[number];
+      ? _menus.map((m, i) => {
+          return {
+            key: i,
+            label: m.title,
+            onClick: () => {
+              if ('target' in m) {
+                const { target } = m;
+                if (target !== pathname) {
+                  navigate(target);
+                }
+              }
+            },
+          } as NonNullable<MenuProps['items']>[number];
         })
       : [];
-  }, []);
+  }, [pathname, navigate]);
 
   return <Menu items={items} {...props}></Menu>;
 }
