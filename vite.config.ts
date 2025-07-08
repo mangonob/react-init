d66bb2ca96ffe223bb0923821676ad87c7908e96
mangonob <mangonob@qq.com>
2025-07-09 01:47:44 +0800

    feat: inspect

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,13 @@ import ViteYaml from '@modyfi/vite-plugin-yaml';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
+import Inspect from 'vite-plugin-inspect';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig((env) => {
   return {
-    plugins: [react(), tsconfigPaths(), ViteYaml()],
+    plugins: [react(), tsconfigPaths(), Inspect(), ViteYaml()],
     css: {
       modules: {
         localsConvention: 'camelCaseOnly',
